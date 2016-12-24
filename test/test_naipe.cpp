c7fd9e416de4456fd8cf273e0bdcbe2c602a7230
Juan Cabrera <jjcp.91@gmail.com>
2016-12-23 23:14:03 -0300

    Agrega test para trc::Naipe{}

--- test/test_naipe.cpp
+++ test/test_naipe.cpp
@@ -7,6 +7,14 @@ using trc::Palo;
 
 TEST_CASE("Naipe")
 {
+    SECTION("Naipe nulo")
+    {
+        Naipe naipe;
+
+        REQUIRE(naipe.valor() == 0);
+        REQUIRE(naipe.palo() == Palo::Nada);
+    }
+
     Naipe naipe{1, Palo::Oro};
 
     REQUIRE(naipe.valor() == 1);
