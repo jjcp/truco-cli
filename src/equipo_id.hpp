8eeb12f8b63f0fee402b621d6931296c208d028b
Juan Cabrera <jjcp.91@gmail.com>
2016-12-23 23:54:59 -0300

    Agrega enumerado trc::EquipoId

---
+++ src/equipo_id.hpp
@@ -0,0 +1,15 @@
+#ifndef TRC_EQUIPO_ID_HPP_Y9IAJHT2
+#define TRC_EQUIPO_ID_HPP_Y9IAJHT2
+
+
+namespace trc {
+
+enum class EquipoId {
+    Nada,
+    E1,   /// Equipo 1
+    E2,   /// Equipo 2
+};
+
+} /* trc */
+
+#endif
