1dea3c13bc7fc7e729b62cceab660962899c4916
Juan Cabrera <jjcp.91@gmail.com>
2016-12-24 15:16:10 -0300

    Agrega clase trc::Jugador

---
+++ src/jugador.hpp
@@ -0,0 +1,27 @@
+#ifndef TRC_JUGADOR_HPP_FZ8KTA4G
+#define TRC_JUGADOR_HPP_FZ8KTA4G
+
+#include "equipo_id.hpp"
+
+
+namespace trc {
+
+class Jugador {
+public:
+
+    Jugador(const trc::EquipoId equipo)
+        : m_equipo{equipo}
+    { }
+
+    auto equipo() const
+    {
+        return m_equipo;
+    }
+
+private:
+    trc::EquipoId m_equipo;
+};
+
+} /* trc */
+
+#endif
