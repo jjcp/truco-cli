b0d2214d61d04e7bd83db7120b7dd751140a9633
Juan Cabrera <jjcp.91@gmail.com>
2016-12-23 21:59:34 -0300

    Elimina repetición de código en trc::Crupier

--- src/crupier.hpp
+++ src/crupier.hpp
@@ -16,7 +16,6 @@ public:
     Crupier()
         : m_prng{m_rng()}
     {
-        m_baraja.reserve(40);
         mezclar();
     }
 
@@ -40,26 +39,29 @@ private:
     std::mt19937            m_prng; /// pseudorandom number generator.
     std::vector<trc::Naipe> m_baraja;
 
+    void insertar_naipes(int valor)
+    {
+        m_baraja.emplace_back(valor, trc::Palo::Oro);
+        m_baraja.emplace_back(valor, trc::Palo::Copa);
+        m_baraja.emplace_back(valor, trc::Palo::Espada);
+        m_baraja.emplace_back(valor, trc::Palo::Basto);
+    }
+
     void inicializar_baraja()
     {
         if (m_baraja.size() != 40)
         {
             m_baraja.clear();
+            m_baraja.reserve(40);
 
             for(int i=1; i <= 7; ++i)
             {
-                m_baraja.emplace_back(i, trc::Palo::Oro);
-                m_baraja.emplace_back(i, trc::Palo::Copa);
-                m_baraja.emplace_back(i, trc::Palo::Espada);
-                m_baraja.emplace_back(i, trc::Palo::Basto);
+                insertar_naipes(i);
             }
 
             for(int i=10; i <= 12; ++i)
             {
-                m_baraja.emplace_back(i, trc::Palo::Oro);
-                m_baraja.emplace_back(i, trc::Palo::Copa);
-                m_baraja.emplace_back(i, trc::Palo::Espada);
-                m_baraja.emplace_back(i, trc::Palo::Basto);
+                insertar_naipes(i);
             }
         }
     }
