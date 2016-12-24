95f8c4ec7c3c4828ce46367173e04f12b6b487d9
Juan Cabrera <jjcp.91@gmail.com>
2016-12-23 23:19:25 -0300

    Agrega más tests para trc::Naipe

--- test/test_naipe.cpp
+++ test/test_naipe.cpp
@@ -15,8 +15,78 @@ TEST_CASE("Naipe")
         REQUIRE(naipe.palo() == Palo::Nada);
     }
 
-    Naipe naipe{1, Palo::Oro};
+    SECTION("Naipe(valor, palo)")
+    {
+        // -------------------
+        // Del 1 al 7
+        // -------------------
 
-    REQUIRE(naipe.valor() == 1);
-    REQUIRE(naipe.palo() == Palo::Oro);
+        for(int i=1; i <= 7; ++i)
+        {
+            Naipe naipe{i, Palo::Oro};
+
+            REQUIRE(naipe.valor() == i);
+            REQUIRE(naipe.palo() == Palo::Oro);
+        }
+
+        for(int i=1; i <= 7; ++i)
+        {
+            Naipe naipe{i, Palo::Copa};
+
+            REQUIRE(naipe.valor() == i);
+            REQUIRE(naipe.palo() == Palo::Copa);
+        }
+
+        for(int i=1; i <= 7; ++i)
+        {
+            Naipe naipe{i, Palo::Espada};
+
+            REQUIRE(naipe.valor() == i);
+            REQUIRE(naipe.palo() == Palo::Espada);
+        }
+
+        for(int i=1; i <= 7; ++i)
+        {
+            Naipe naipe{i, Palo::Basto};
+
+            REQUIRE(naipe.valor() == i);
+            REQUIRE(naipe.palo() == Palo::Basto);
+        }
+
+        // -------------------
+        // Del 10 al 12
+        // -------------------
+
+        for(int i=10; i <= 12; ++i)
+        {
+            Naipe naipe{i, Palo::Oro};
+
+            REQUIRE(naipe.valor() == i);
+            REQUIRE(naipe.palo() == Palo::Oro);
+        }
+
+        for(int i=10; i <= 12; ++i)
+        {
+            Naipe naipe{i, Palo::Copa};
+
+            REQUIRE(naipe.valor() == i);
+            REQUIRE(naipe.palo() == Palo::Copa);
+        }
+
+        for(int i=10; i <= 12; ++i)
+        {
+            Naipe naipe{i, Palo::Espada};
+
+            REQUIRE(naipe.valor() == i);
+            REQUIRE(naipe.palo() == Palo::Espada);
+        }
+
+        for(int i=10; i <= 12; ++i)
+        {
+            Naipe naipe{i, Palo::Basto};
+
+            REQUIRE(naipe.valor() == i);
+            REQUIRE(naipe.palo() == Palo::Basto);
+        }
+    }
 }
